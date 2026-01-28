e07a2ab8499cfa2d1eec1cca8eaff4282432ae92
jerj433 <jeremyk.joe@gmail.com>
2026-01-27 21:57:48 -0800

    Create session2.py

---
+++ session2.py
@@ -0,0 +1,25 @@
+def main():
+
+    a = 3.5
+    b = 2.1
+    s = a + b
+    print("sum:", s)
+    print("type:", type(s))
+
+   
+    x = 17
+    y = 5
+    d = x - y
+    print("difference:", d)
+    print("type:", type(d))
+
+ 
+    m = 4.0
+    n = 6
+    p = m * n
+    print("product:", p)
+    print("type:", type(p))
+
+
+if __name__ == "__main__":
+    main()
